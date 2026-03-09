f476b315c330a6011c69dcae62b0f0f0665ed521
Nektoo <slivanikita06@gmail.com>
2026-03-09 16:10:38 +0200

    Updated Wiki

--- docs/components/Item.vue
+++ docs/components/Item.vue
@@ -2,7 +2,10 @@
   <div style="display: flex; align-items: center">
     <img :src="Book" style="width: 16px; height: 16px" />
     <div style="margin-left: 7px"><slot></slot></div>
-    <img :src="Level" :style="{width: `${W}px`, height: "16px", marginLeft: "7px"}" />
+    <img
+      :src="Level"
+      :style="{ width: `${W}px`, height: `12px`, marginLeft: `7px` }"
+    />
   </div>
 </template>
 
