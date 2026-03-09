c2324c83baa984dae3aeacafb590ab23c1703f37
Nektoo <slivanikita06@gmail.com>
2026-03-09 16:04:45 +0200

    Updated Wiki

---
+++ docs/components/Item.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="display: flex; align-items: center">
+    <img :src="Book" style="width: 16px; height: 16px" />
+    <div style="margin-left: 7px"><slot></slot></div>
+    <img :src="Level" :style="{width: `${W}px`, height: "16px", marginLeft: "7px"}" />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  W: Number,
+  Book: String,
+  Level: String,
+});
+</script>
