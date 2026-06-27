69da95df11cada99e5ccbcf0a6a32fe697553841
Nektoo <slivanikita06@gmail.com>
2026-06-27 15:25:50 +0300

    Updated Wiki (RS)

--- rspress.config.ts
+++ rspress.config.ts
@@ -4,16 +4,10 @@ import { defineConfig } from '@rspress/core';
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   lang: 'ru',
-  title: 'MINECRAFT СНГ | WIKI',
-  icon: '/rspress-icon.png',
+  title: 'MINECRAFT СНГ WIKI',
+  icon: '/icon.png',
   logoText: 'MINECRAFT СНГ',
   themeConfig: {
-    socialLinks: [
-      {
-        icon: 'github',
-        mode: 'dom',
-        content: '<a href="https://penis.com/">penis</a>',
-      },
-    ],
+    socialLinks: [],
   },
 });
